96d4ecd625799c58ad272deb2f6ba2377c13332d
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2024-06-24 17:56:11 +0300

    make carousel slider

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -1,43 +1,38 @@
 import Carousel from 'react-bootstrap/Carousel';
+import { Link } from 'react-router-dom';
 
 function CarouselSlider() {
   return (
     <Carousel>
       <Carousel.Item>
-        <img
-          className="carousel-image"
-          src="./img/slider1.jpg"
-          text="Second slide"
-          alt="image_slider"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <Link to="/allbrands">
+          <img
+            className="carousel-image"
+            src="./img/rung.png"
+            text="Second slide"
+            alt="image_slider"
+          />
+        </Link>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="carousel-image"
-          src="./img/slider2.jpg"
-          text="Second slide"
-          alt="image_slider"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <Link to="/homeproduct">
+          <img
+            className="carousel-image"
+            src="./img/home.png"
+            text="Second slide"
+            alt="image_slider"
+          />
+        </Link>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="carousel-image"
-          src="./img/slider3.jpg"
-          text="Third slide"
-          alt="image_slider"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
+        <Link to="/animals">
+          <img
+            className="carousel-image"
+            src="./img/animal.png"
+            text="Third slide"
+            alt="image_slider"
+          />
+        </Link>
       </Carousel.Item>
     </Carousel>
   );
--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -4,15 +4,29 @@ import Sale from './Sale/Sale';
 import Image from './Image/Image';
 import Video from './VideoPlayer/Video';
 import SearchSection from './SearchSection/SearchSection';
+import CarouselSlider from './CarouselSlider';
 
 function HomeContent() {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <Video />
+      {windowWidth < 600 ? <CarouselSlider /> : <Video />};
       <SearchSection />
       <Promo />
       <Sale />
