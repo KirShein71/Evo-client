9eacfbd41a4f172ed1be7a53f476f8677babe848
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2024-08-01 20:58:03 +0300

    update admin order

--- src/pages/Admin/AdminOrder/AdminOrder.jsx
+++ src/pages/Admin/AdminOrder/AdminOrder.jsx
@@ -7,7 +7,6 @@ import UpdateOrder from './modals/updateOrder';
 import UpdatePhone from './modals/UpdatePhone';
 import UpdateDelivery from './modals/UpdateDelivery';
 import CreateOrder from './modals/CreateOrder';
-import { Moment } from 'react-moment';
 import { CSVLink } from 'react-csv';
 
 import './style.scss';
@@ -299,9 +298,7 @@ const AdminOrder = () => {
                   <li>Полная стоимость заказа (без доставки): {item.order.totalamount}</li>
                 </ul>
               </td>
-              <td>
-                <Moment format="DD.MM.YYYY">{item.createdAt}</Moment>
-              </td>
+              <td>{new Date(item.createdAt).toLocaleDateString('ru-RU')}</td>
               <td
                 style={{
                   cursor: 'pointer',
