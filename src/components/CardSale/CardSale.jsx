b3639aee28a448c491220323f47182cb88bd64a2
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2024-08-15 12:22:52 +0300

    update card sale

--- src/components/CardSale/CardSale.jsx
+++ src/components/CardSale/CardSale.jsx
@@ -9,7 +9,7 @@ function CardSale({ image, name, old_price, new_price }) {
   const location = useLocation();
 
   const addToOneProduct = () => {
-    const formattedName = originalName.replace(/\s+/g, '-').toLowerCase(); // Форматируем имя для URL
+    const formattedName = originalName.replace(/-+/g, '--').replace(/\s+/g, '-');
     navigate(`/productproperty/${formattedName}`, {
       state: { from: location.pathname, originalName },
     });
