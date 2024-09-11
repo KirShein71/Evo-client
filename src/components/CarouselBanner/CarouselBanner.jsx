1ec68bdf8adc9742cc7486e29a67dcb41617bf9e
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2024-09-11 15:31:59 +0300

    fix

--- src/components/CarouselBanner/CarouselBanner.jsx
+++ src/components/CarouselBanner/CarouselBanner.jsx
@@ -1,71 +1,39 @@
-import React from 'react';
-import Slider from 'react-slick';
-
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import Carousel from 'react-bootstrap/Carousel';
 import { Link } from 'react-router-dom';
-import './style.scss';
 
 function CarouselBanner() {
-  const [currentSlide, setCurrentSlide] = React.useState(0);
-  const sliderRef = React.useRef(null);
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    pauseOnHover: true,
-  };
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      if (sliderRef.current) {
-        sliderRef.current.slickNext();
-      }
-    }, settings.autoplaySpeed);
-
-    return () => clearInterval(interval);
-  }, []);
   return (
     <div className="banner">
-      <div style={{ width: '90%' }}>
-        <Slider {...settings} ref={sliderRef}>
-          <div className="banner__slide">
-            <Link to="/allbrands">
-              <img
-                className="banner-image"
-                src="./img/rug.png"
-                text="Second slide"
-                alt="image_slider"
-              />
-            </Link>
-          </div>
-          <div className="banner__slide">
-            <Link to="/homeproduct">
-              <img
-                className="banner-image"
-                src="./img/bag.png"
-                text="Second slide"
-                alt="image_slider"
-              />
-            </Link>
-          </div>
-          <div className="banner__slide">
-            <Link to="/homeproduct">
-              <img
-                className="banner-image"
-                src="./img/delivery.png"
-                text="Second slide"
-                alt="image_slider"
-              />
-            </Link>
-          </div>
-        </Slider>
-      </div>
+      <Carousel>
+        <Carousel.Item>
+          <Link to="/allbrands">
+            <img
+              className="banner__image"
+              src="./img/rug.png"
+              text="Second slide"
+              alt="image_slider"
+            />
+          </Link>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Link to="/accessories">
+            <img
+              className="banner__image"
+              src="./img/bag.png"
+              text="Second slide"
+              alt="image_slider"
+            />
+          </Link>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="banner__image"
+            src="./img/delivery.png"
+            text="Third slide"
+            alt="image_slider"
+          />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
